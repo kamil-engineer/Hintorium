c44b1d37a4c1fe2c7128eb2835995cc5fe436ade
Kamil <mathey.academy@gmail.com>
2025-11-12 20:13:51 +0100

    fix(exporting): export newly added functionalities

--- packages/hintorium/src/index.ts
+++ packages/hintorium/src/index.ts
@@ -1,6 +1,7 @@
 import "./style.css";
-import { HintoriumTour } from "./tooltip/tour";
 export { initTooltip } from "./tooltip";
+export { InlineHint } from "./tooltip/inline-hint";
+export { HintoriumTour } from "./tooltip/tour";
 export {
   type TooltipTheme,
   type TooltipAnimation,
@@ -9,34 +10,3 @@ export {
 } from "./tooltip/types";
 
 export { Tooltip } from "./tooltip/tooltip";
-
-new HintoriumTour({
-  steps: [
-    {
-      target: "#menu",
-      content: "This is the main menu of the application.",
-      options: {
-        theme: "gradient",
-      },
-    },
-    {
-      target: "#search",
-      content: "This is the serach menu of the application.",
-      options: {
-        theme: "gradient",
-      },
-    },
-    {
-      target: "#save-btn",
-      content: "Click this button to save your changes.",
-      options: {
-        theme: "gradient",
-      },
-    },
-  ],
-  localStorageKey: "hintorium_tour_examplee",
-  auto: {
-    enabled: true,
-    delay: 3000,
-  },
-}).start();
