d3e842f33d16df3dce1a3743f010fc3679ced50b
Kamil <mathey.academy@gmail.com>
2025-11-24 22:16:23 +0100

    feat: body scroll lock

--- apps/docs/src/logic/mobile-navigation.ts
+++ apps/docs/src/logic/mobile-navigation.ts
@@ -21,6 +21,12 @@ export const handleMobileNavigation = () => {
       isOpen ? "Close menu" : "Open menu"
     );
 
+    if (isOpen) {
+      document.body.classList.add("body--no-scroll");
+    } else {
+      document.body.classList.remove("body--no-scroll");
+    }
+
     if (!buttonIcon) return;
 
     buttonIcon.setAttribute("src", isOpen ? closeIcon : openIcon);
@@ -34,6 +40,8 @@ export const handleMobileNavigation = () => {
       toggleButton?.setAttribute("aria-expanded", "false");
       toggleButton?.setAttribute("aria-label", "Open menu");
       buttonIcon?.setAttribute("src", openIcon);
+
+      document.body.classList.remove("body--no-scroll");
     });
   });
 };
